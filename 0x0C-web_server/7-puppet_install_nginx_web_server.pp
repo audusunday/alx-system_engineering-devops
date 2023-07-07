25677dbf9fe6add75af4d1f414758c93f5540293
audusunday <sunnadu13@gmail.com>
2023-07-07 18:04:40 +0100

    nginx configuration using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,17 @@
 # automate all task using puppet
 
-package {'nginx':
-  ensure => 'present',
+package { 'nginx':
+  ensure => installed,
 }
 
-file_line {'instal':
+file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.github.com/audusunday permanent;',
 }
 
-file {'/var/www/html/index.html':
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
