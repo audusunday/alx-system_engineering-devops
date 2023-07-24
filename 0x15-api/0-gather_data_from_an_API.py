a85ae59f4905ed3e448cf3d07d6dee0f98b4a9cd
audusunday <sunnadu13@gmail.com>
2023-07-24 16:14:54 +0100

    new update on code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,31 @@
 #!/usr/bin/python3
-# Apython script that uses RESTful Api to give information about user's TO DO LIST
+"""Accessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todo_url = f'{base_url}/todos?userId={employee_id}'
-
-    try:
-        user_response = requests.get(user_url)
-        todo_response = requests.get(todo_url)
-
-        user_data = user_response.json()
-        todo_data = todo_response.json()
-
-        if not user_data:
-            print(f"Employee with ID {employee_id} not found.")
-            return
-
-        employee_name = user_data['name']
-
-        total_tasks = len(todo_data)
-        completed_tasks = sum(1 for task in todo_data if task['completed'])
-
-        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
-
-        for task in todo_data:
-            if task['completed']:
-                print(f"\t{task['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
-        sys.exit(1)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script.py EMPLOYEE_ID")
-        sys.exit(1)
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
